631040c15ad0d9cbec30abc5d6888f90f65063b6
river-ceanne <snvencer@gmail.com>
2019-07-02 13:07:30 -0700

    post mapping route made

--- src/main/java/com/reina/taskmaster/TaskController.java
+++ src/main/java/com/reina/taskmaster/TaskController.java
@@ -1,8 +1,11 @@
 package com.reina.taskmaster;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
 import java.text.ParseException;
@@ -13,6 +16,20 @@ public class TaskController {
 
     @Autowired
     TaskRepository taskRepository;
+
+    private DynamoDBMapper dynamoDBMapper;
+
+    @GetMapping("/tasks")
+    public String getTenantAddPage() {
+        return "tenant";
+    }
+
+    @PostMapping("/tasks")
+    public void createTenant( String title, String description) throws ParseException {
+        Task task = new Task(title,description);
+        taskRepository.save(task);
+    }
+
 
 
     @PutMapping("/tasks/{id}/state")
