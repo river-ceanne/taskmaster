af89e309e0dfb853b5f78c38113e59b57f816af8
river-ceanne <snvencer@gmail.com>
2019-07-03 10:44:39 -0700

    added routes to get tasks of a user and modified POST to create an assigned task

--- src/main/java/com/reina/taskmaster/TaskRepository.java
+++ src/main/java/com/reina/taskmaster/TaskRepository.java
@@ -3,6 +3,7 @@ package com.reina.taskmaster;
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -12,6 +13,7 @@ public interface TaskRepository extends CrudRepository<Task, String> {
 
 //    Optional<Task> findById(UUID id);
     Task findById(UUID id);
+    List<Task> findByAssignee(String name);
 
 
 }
